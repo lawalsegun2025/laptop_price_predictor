1292ff6ec97e6b6ca695c5dcc882414c5182409b
lawalsegun2025 <lawalsegun2025@gmail.com>
2023-05-28 05:13:13 +0100

    still building

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ file1 =  open("pipeline.pkl", "rb")
 rf = pickle.load(file1)
 file1.close()
 
+# Company,TypeName,Ram,OpSys,Weight,Touchscreen,IPS,PPI,CPU_name,HDD,SSD,Gpu brand
 # Apple,Ultrabook,8,Mac,1.37,0,1,226.98300468106115,Intel Core i5,0,128,Intel
 
 data = pd.read_csv("cleaned_X_train_data.csv")
